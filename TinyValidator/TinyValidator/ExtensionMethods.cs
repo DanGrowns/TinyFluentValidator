1839ed2f19808dc76f989e50baee57eedb492a73
Dan <dgrowns@gmail.com>
2021-10-29 08:01:02 +0100

    Import

---
+++ TinyValidator/TinyValidator/ExtensionMethods.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TinyValidator
+{
+    public static class ExtensionMethods
+    {
+        public static string SplitPascalCaseToString(this string pascalCaseStr, bool usePluralName = false)
+        {
+            var r = new Regex(@"
+                (?<=[A-Z])(?=[A-Z][a-z]) |
+                 (?<=[^A-Z])(?=[A-Z]) |
+                 (?<=[A-Za-z])(?=[^A-Za-z])", RegexOptions.IgnorePatternWhitespace);
+
+            var modelWithSpaces = r.Replace(pascalCaseStr, " ");
+            var finalChar = pascalCaseStr[^1..];
+            
+            if (usePluralName)
+                return finalChar == "s" ? $"{modelWithSpaces}es" : $"{modelWithSpaces}s";
+
+            return modelWithSpaces;
+        }
+    }
+}
---
+++ TinyValidator/TinyValidator/Validations.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace TinyValidator
+{
+    public interface IValidator<T>
+    {
+        IValidator<T> Start(T item);
+        IValidator<T> RuleFor(Expression<Func<T, string>> expression);
+        IValidator<T> NotEmpty(string errorMessage = "[PropertyName] cannot be empty.");
+        IReadOnlyList<string> ToList();
+    }
+    
+    // TODO: Preventing things like RuleFor being called twice in a row.
+    // Make sure all state is cleared if it is called incorrectly in a chain
+    public class Validations<T> : IValidator<T>
+    {
+        private List<string> Errors { get; }
+        private T ValidationObject { get; set; }
+        public string MemberName { get; set; }
+        public object MemberValue { get; set; }
+
+        public Validations()
+        {
+            Errors = new List<string>();
+        }
+        
+        public IValidator<T> RuleFor(Expression<Func<T, string>> expression)
+        {
+            var body = (MemberExpression) expression.Body;
+
+            MemberName = body.Member.Name.SplitPascalCaseToString();
+            MemberValue = expression.Compile().Invoke(ValidationObject);
+
+            return this;
+        }
+        
+        public IValidator<T> NotEmpty(string errorMessage = "[PropertyName] cannot be empty.")
+        {
+            if (string.IsNullOrEmpty(MemberValue.ToString()))
+            {
+                errorMessage = errorMessage.Replace($"[PropertyName]", $"'{MemberName}'");
+                Errors.Add(errorMessage);
+            }
+
+            return this;
+        }
+
+        public IValidator<T> Start(T item)
+        {
+            ValidationObject = item;
+            return this;
+        }
+        
+        public IReadOnlyList<string> ToList() => Errors;
+    }
+}
